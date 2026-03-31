710deff8bb205914273603c0f3f3c5aaa9cbc53f
Diego Herreros <diegohp85@gmail.com>
2026-03-31 20:44:40 +0200

    configuracion del gradle

---
+++ src/test/java/co_dhp85/SampleTest.java
@@ -0,0 +1,12 @@
+package co_dhp85;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SampleTest {
+    @Test
+    void sampleTestSerenity(){
+        assertTrue(true, "Mensaje exitoso");
+    }
+}
